65e75c791a28a60392b3bbdae339597c53ae2036
André Ribeiro <andremezzaliraribeiro@gmail.com>
2021-02-25 14:59:14 -0300

    Att do index

--- index.php
+++ index.php
@@ -10,32 +10,4 @@ use App\Comunication\TelegramBot;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-<<<<<<< HEAD
-Call::getLastMessage();
-=======
-//DEFINE THE ALERT TIME
-$message_time = '00:00:00';
-
-while (true) {
-    // SET YOUR CURRENT TIME ZONE
-    //SEARCH FOR MORE INFORMATION AT 'WATHER API' DOCUMENTATION
-    date_default_timezone_set('America/Sao_Paulo');
-
-    
-    //SET THE CURRENT TIME
-    $current_time = date('H:i:s');  
-
-    
-    if ($current_time == $message_time) {
-        
-        $weather = new Weather();
-        $weather_result = [];
-        $weather_result = $weather->getWeatherByCity('YOUR_CITY_NAME');
-        Call::sendWeather($weather_result,'YOUR_TELEGRAM_ID');
-    
-    }
-    
-    
-
-}
->>>>>>> 9f3dd70fb11f51d8b8d748f0cb57d072202cb3a5
+Call::getMessage();
